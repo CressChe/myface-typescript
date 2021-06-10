fbedffc198b384c0dd79ab21acd2f3accb0e72e7
Cressida Chesworth <Cressida.Chesworth@softwire.com>
2021-06-10 15:46:08 +0100

    added javascript

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,36 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Add your JavaScript here.
+document.addEventListener('DOMContentLoaded', function () {
+
+    const randomiser = document.getElementById('randomColorButton');
+    const postsToRandomise = document.getElementsByClassName('listedPosts');
+    randomiser.addEventListener('click', function () {
+        for (let i = 0; i < postsToRandomise.length; i++) {
+            postsToRandomise[i].style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+        }
+    })
+
+
+    const nav = document.getElementById("nav");
+    document.onkeydown = function(keyPress) {
+        if (keyPress.keyCode == 27) {
+            nav.className = "header";
+        }
+    };
+
+    const burgerMenu = document.getElementsByClassName("burgerMenuContainer");
+    console.log(burgerMenu);
+    burgerMenu[0].addEventListener("click", function (x) {
+        myFunction(this);
+    
+    })
+
+    const myFunction = function(x) {
+        x.classList.toggle("change");
+        if (nav.className === "header") {
+            nav.className = 'displayHeader';
+        } else if (nav.className === "displayHeader") {
+            nav.className = "header";
+        }
+      }
+    
+
 });
